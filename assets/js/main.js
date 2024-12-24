013ece456a1712e95d9710d22802c2835d41048a
anas nashat <72121781+anasnashat@users.noreply.github.com>
2024-12-24 21:10:40 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,21 +7,46 @@ const startSection = document.querySelector("#startExam");
 let exam = new ExamController(examPage, nextButton);
 
 startSection.querySelector("button").addEventListener('click', () => {
-    Swal.fire(
-        {
-            title: "Hi, What is your name?",
-            content: "input",
-        })
-        .then((value) => {
-                if (value.trim() !== "") {
-                    document.querySelector("#stName").innerHTML += value;
-                    startSection.classList.add("d-none");
-                    examPage.classList.remove("d-none");
-                    exam.displayQuestion();
 
-                }
-            }
-        );
+    Swal.fire({
+        title: "Hi, What is your name?",
+        input: "text",
+        inputPlaceholder: "Enter your name",
+        confirmButtonText: "Start The Exam",
+        backdrop: true,
+        allowOutsideClick: false,
+        customClass: {
+            popup: "swal-modal",
+            title: "swal-title",
+            htmlContainer: "swal-text",
+            input: "swal-input",
+            confirmButton: "swal-button",
+            cancelButton: "swal-button",
+        },
+    }).then((result) => {
+        if (result.value.trim() !== "") {
+            document.querySelector("#stName").innerHTML += result.value;
+            startSection.classList.add("d-none");
+            examPage.classList.remove("d-none");
+            exam.displayQuestion();
+        } else  {
+            Swal.fire({
+                title: "Error!",
+                text: "you Should to Enter yor name to start the exam ?  ",
+                icon: "error",
+                customClass: {
+                    popup: "swal-modal",
+                    title: "swal-title",
+                    htmlContainer: "swal-text",
+                    input: "swal-input",
+                    confirmButton: "swal-button",
+                    cancelButton: "swal-button",
+                },
+
+            });
+        }
+    });
+
 })
 
 
